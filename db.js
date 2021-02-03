0fb32e3f0f93302aac74e16a363ee4c60e8d9bf5
Peyton Nix <peytonnix50@gmail.com>
2021-02-02 20:02:49 -0600

    heroku stuff

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-// import sequelize package
 const Sequelize = require('sequelize');
-//connect to database using sequelize where
-//                                 db name          user           password     options
+
 const database = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    dialect: 'postgres',
+    host: 'localhost'
 });
-//authenticate using the username and password
+
 database.authenticate()
     .then(() => console.log('postgres db is connected'))
     .catch(err => console.log(err));
